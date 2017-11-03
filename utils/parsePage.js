019d89a5b1803b412ee14cb74ca6771fae2aa94c
Joseph <joseph@Josephs-MacBook-Pro.local>
2017-11-02 19:28:50 -0700

    Grabbing the review rating and count from the page.

--- utils/parsePage.js
+++ utils/parsePage.js
@@ -1,4 +1,18 @@
 const cheerio = require("cheerio");
 module.exports = page => {
-  //
+  try {
+    const $ = cheerio.load(page);
+    const rating = $(".rating-info .i-stars")
+      .attr("title")
+      .trim()
+      .split(" ")[2];
+    const reviewCount = $(".rating-info .review-count")
+      .text()
+      .trim()
+      .split(" ")[0];
+
+    console.log(rating, reviewCount);
+  } catch (error) {
+    return Promise.reject(`Error Parsing Page ${JSON.stringify(error)}`);
+  }
 };
